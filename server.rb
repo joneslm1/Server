22eff144c48063c22a82205205c396ec9ac1161f
Lewis Jones <lewismjones@gmail.com>
2017-03-01 11:35:34 -0500

    third commit

--- server.rb
+++ server.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
 
-get '/' do
-  "Hello world"
-end
 
-get "/sinatra" do
-  "Hello Sinatra"
-end
+  get '/' do
+    current_time = Time.now.wday
+    weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    "Hello! Today is #{weekdays[current_time]}"
+  end
+
+  get "/sinatra" do
+    "Hello Sinatra"
+  end
