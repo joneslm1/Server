a5bb8f16e595033b155dc723ec3467f818064ed7
Lewis Jones <lewismjones@gmail.com>
2017-03-01 22:32:32 -0500

    Updates server to change root page

--- server.rb
+++ server.rb
@@ -1,13 +1,20 @@
 require 'sinatra'
 
-
   get '/' do
-    current_time = Time.now.wday
-    weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-    "Hello! Today is #{weekdays[current_time]}"
+  File.read('public/hello.txt')
   end
+
+
+    # current_time = Time.now.wday
+    # weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    #
+    # "Hello! Today is #{weekdays[current_time]}"
+
 
   get "/sinatra" do
     "Hello Sinatra"
   end
+
+get "/hello.txt" do
+  'hello.txt'
+end
